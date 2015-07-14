ba04cd986ed44e36326af9e807c6083ef623d9be
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-14 15:21:54 -0300

    aaa

--- public/catalog/language/portuguese-br/module/cart.php
+++ public/catalog/language/portuguese-br/module/cart.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'Carrinho';
 
 // Text
-$_['text_items']    = '%s item(s) - %s';
+$_['text_items']    = '%s';
 $_['text_empty']    = 'Seu carrinho está vazio!';
 $_['text_cart']     = 'Ver carrinho';
 $_['text_checkout'] = 'Finalizar compra';
